29eddbe92a1e3ce65631344cdd26b4ddbf10f0e1
David Pascual <david.pascualh@estudiante.uam.es>
2019-12-03 02:34:23 +0100

    Consulta clientesDistintos

---
+++ public_html/app/SQL/clientesDistintos.sql
@@ -0,0 +1,17 @@
+﻿SELECT
+	C.customerid,
+	firstname,
+	lastname,
+	SUM(totalamount) ta
+FROM
+	customers C
+	JOIN orders o ON C.customerid = o.customerid
+WHERE
+	orderdate < '2015-05-1'
+	AND orderdate >= '2015-04-01'
+GROUP BY
+	C.customerid,
+	firstname,
+	lastname
+HAVING
+	SUM(totalamount) > 100
