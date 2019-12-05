b5653c2edb750f5f3510b5f2dde46c2691af8daa
AMessina2 <alain.messina@estudiante.uam.es>
2019-12-05 18:33:45 +0100

    clientesDistintos

--- public_html/app/SQL/clientesDistintos.sql
+++ public_html/app/SQL/clientesDistintos.sql
@@ -1,21 +1,21 @@
 ﻿--create index idx_orderdate on orders(orderdate)
 --
 
-EXPLAIN
-SELECT
+--EXPLAIN
+SELECT COUNT(*)
+FROM (
+SELECT DISTINCT
 	C.customerid,
 	firstname,
-	lastname,
-	SUM(totalamount) ta
+	lastname
 FROM
 	customers C
 	JOIN orders o ON C.customerid = o.customerid
 WHERE
 	orderdate < '2015-05-1'
 	AND orderdate >= '2015-04-01'
+	AND totalamount > 100
 GROUP BY
 	C.customerid,
 	firstname,
-	lastname
-HAVING
-	SUM(totalamount) > 100
+	lastname) as A
