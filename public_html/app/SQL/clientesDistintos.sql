ca657dc3107ed12cfd888d1284639fcb5bbfc58b
Your Name <you@example.com>
2019-12-05 19:29:48 +0100

    clase

--- public_html/app/SQL/clientesDistintos.sql
+++ public_html/app/SQL/clientesDistintos.sql
@@ -1,13 +1,13 @@
-﻿--create index idx_orderdate on orders(orderdate)
---
+﻿--create index idx_orderdate on orders(orderdate);
+--drop index if exists idx_totalamount;
+--create index idx_totalamount on orders(totalamount);
+--drop index idx_orderdate;
+--drop index idx_doble;
+create index idx_doble2 on orders(totalamount, orderdate);
 
---EXPLAIN
-SELECT COUNT(*)
-FROM (
-SELECT DISTINCT
-	C.customerid,
-	firstname,
-	lastname
+
+EXPLAIN
+SELECT COUNT(DISTINCT C.customerid) as NumClientes
 FROM
 	customers C
 	JOIN orders o ON C.customerid = o.customerid
@@ -15,7 +15,3 @@ WHERE
 	orderdate < '2015-05-1'
 	AND orderdate >= '2015-04-01'
 	AND totalamount > 100
-GROUP BY
-	C.customerid,
-	firstname,
-	lastname) as A
--- public_html/app/SQL/planes.sql
+++ public_html/app/SQL/planes.sql
@@ -1,32 +1,72 @@
 -- clientesDistintos 
- Finalize GroupAggregate  (cost=4986.78..5077.99 rows=909 width=272)
-   Group Key: c.customerid
-   Filter: (sum(o.totalamount) > '100'::numeric)
-   ->  Gather Merge  (cost=4986.78..5057.67 rows=535 width=272)
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------
+ Aggregate  (cost=5240.13..5240.14 rows=1 width=8)
+   ->  Gather  (cost=1000.28..5239.37 rows=303 width=4)
          Workers Planned: 1
-         ->  Partial GroupAggregate  (cost=3986.77..3997.47 rows=535 width=272)
-               Group Key: c.customerid
-               ->  Sort  (cost=3986.77..3988.11 rows=535 width=272)
-                     Sort Key: c.customerid
-                     ->  Hash Join  (cost=670.09..3962.53 rows=535 width=272)
-                           Hash Cond: (o.customerid = c.customerid)
-                           ->  Parallel Seq Scan on orders o  (cost=0.00..3291.03 rows=535 width=36)
-                                 Filter: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
-                           ->  Hash  (cost=493.93..493.93 rows=14093 width=240)
-                                 ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=240)
-(15 rows)
+         ->  Nested Loop  (cost=0.29..4209.07 rows=178 width=4)
+               ->  Parallel Seq Scan on orders o  (cost=0.00..3558.37 rows=178 width=4)
+                     Filter: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
+               ->  Index Only Scan using customers_pkey on customers c  (cost=0.29..3.66 rows=1 width=4)
+                     Index Cond: (customerid = o.customerid)
+(8 filas)
+
+
 
 -- clientesDistintos index orderdate
-HashAggregate  (cost=2180.36..2194.00 rows=909 width=272)
-   Group Key: c.customerid
-   Filter: (sum(o.totalamount) > '100'::numeric)
-   ->  Hash Join  (cost=691.83..2173.55 rows=909 width=272)
+----------------------------------------------------------------------------------------------------------------------------
+ Aggregate  (cost=2174.83..2174.84 rows=1 width=8)
+   ->  Hash Join  (cost=691.68..2174.08 rows=303 width=4)
          Hash Cond: (o.customerid = c.customerid)
-         ->  Bitmap Heap Scan on orders o  (cost=21.74..1501.07 rows=909 width=36)
+         ->  Bitmap Heap Scan on orders o  (cost=21.59..1503.19 rows=303 width=4)
                Recheck Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
+               Filter: (totalamount > '100'::numeric)
                ->  Bitmap Index Scan on idx_orderdate  (cost=0.00..21.51 rows=909 width=0)
                      Index Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
-         ->  Hash  (cost=493.93..493.93 rows=14093 width=240)
-               ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=240)
-(11 rows)
+         ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
+               ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
+(10 filas)
+
+
+-- clientesDistintos index totalamount
+-------------------------------------------------------------------------------------------------
+ Aggregate  (cost=4546.07..4546.08 rows=1 width=8)
+   ->  Hash Join  (cost=1797.07..4545.31 rows=303 width=4)
+         Hash Cond: (o.customerid = c.customerid)
+         ->  Bitmap Heap Scan on orders o  (cost=1126.97..3874.42 rows=303 width=4)
+               Recheck Cond: (totalamount > '100'::numeric)
+               Filter: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
+               ->  Bitmap Index Scan on idx_totalamount  (cost=0.00..1126.90 rows=60597 width=0)
+                     Index Cond: (totalamount > '100'::numeric)
+         ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
+               ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
+(10 filas)
+
+-- clientesDistintos index orderdate, totalamount
+-------
+ Aggregate  (cost=1480.42..1480.43 rows=1 width=8)
+   ->  Hash Join  (cost=697.95..1479.66 rows=303 width=4)
+         Hash Cond: (o.customerid = c.customerid)
+         ->  Bitmap Heap Scan on orders o  (cost=27.86..808.78 rows=303 width=4)
+               Recheck Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
+               ->  Bitmap Index Scan on idx_doble  (cost=0.00..27.78 rows=303 width=0)
+                     Index Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
+         ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
+               ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
+(9 filas)
+
+-- clientesDistintos index totalamount, orderdate
+ Aggregate  (cost=3150.52..3150.53 rows=1 width=8)
+   ->  Hash Join  (cost=2368.05..3149.76 rows=303 width=4)
+         Hash Cond: (o.customerid = c.customerid)
+         ->  Bitmap Heap Scan on orders o  (cost=1697.96..2478.88 rows=303 width=4)
+               Recheck Cond: ((totalamount > '100'::numeric) AND (orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
+               ->  Bitmap Index Scan on idx_doble2  (cost=0.00..1697.88 rows=303 width=0)
+                     Index Cond: ((totalamount > '100'::numeric) AND (orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
+         ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
+               ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
+(9 filas)
+
+
+
 
