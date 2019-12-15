b5d0032e86326e87b5556fa9ba9409c5eba480b7
David Pascual <david.pascualh@estudiante.uam.es>
2019-12-15 01:28:06 +0100

    Apartado A done

--- public_html/app/SQL/clientesDistintos.sql
+++ public_html/app/SQL/clientesDistintos.sql
@@ -1,11 +1,10 @@
-﻿--create index idx_orderdate on orders(orderdate);
---drop index if exists idx_totalamount;
---create index idx_totalamount on orders(totalamount);
---drop index idx_orderdate;
---drop index idx_doble;
-create index idx_doble2 on orders(totalamount, orderdate);
+﻿drop index if exists idx_orderdate;
+drop index if exists idx_totalamount;
+drop index if exists idx_orderdate_totalamount;
+drop index if exists idx_totalamount_orderdate;
 
 
+\echo '\nConsulta sin indices añadidos'
 EXPLAIN
 SELECT COUNT(DISTINCT C.customerid) as NumClientes
 FROM
@@ -14,4 +13,61 @@ FROM
 WHERE
 	orderdate < '2015-05-1'
 	AND orderdate >= '2015-04-01'
-	AND totalamount > 100
+	AND totalamount > 100;
+
+
+create index idx_orderdate on orders(orderdate);
+\echo '\nConsulta con indice en orderdate'
+EXPLAIN
+SELECT COUNT(DISTINCT C.customerid) as NumClientes
+FROM
+	customers C
+	JOIN orders o ON C.customerid = o.customerid
+WHERE
+	orderdate < '2015-05-1'
+	AND orderdate >= '2015-04-01'
+	AND totalamount > 100;
+
+
+drop index idx_orderdate;
+create index idx_totalamount on orders(totalamount);
+\echo '\nConsulta con indice en totalamount'
+EXPLAIN
+SELECT COUNT(DISTINCT C.customerid) as NumClientes
+FROM
+	customers C
+	JOIN orders o ON C.customerid = o.customerid
+WHERE
+	orderdate < '2015-05-1'
+	AND orderdate >= '2015-04-01'
+	AND totalamount > 100;
+
+
+drop index idx_totalamount;
+create index idx_orderdate_totalamount on orders(orderdate, totalamount);
+\echo '\nConsulta con indice multicolumna como (orderdate, totalamount)'
+EXPLAIN
+SELECT COUNT(DISTINCT C.customerid) as NumClientes
+FROM
+	customers C
+	JOIN orders o ON C.customerid = o.customerid
+WHERE
+	orderdate < '2015-05-1'
+	AND orderdate >= '2015-04-01'
+	AND totalamount > 100;
+
+
+drop index idx_orderdate_totalamount;
+create index idx_totalamount_orderdate on orders(totalamount, orderdate);
+\echo '\nConsulta con indice multicolumna como (totalamount, orderdate)'
+EXPLAIN
+SELECT COUNT(DISTINCT C.customerid) as NumClientes
+FROM
+	customers C
+	JOIN orders o ON C.customerid = o.customerid
+WHERE
+	orderdate < '2015-05-1'
+	AND orderdate >= '2015-04-01'
+	AND totalamount > 100;
+
+drop index idx_totalamount_orderdate;
--- public_html/app/SQL/planes_clientesDistintos.sql
+++ public_html/app/SQL/planes_clientesDistintos.sql
@@ -1,5 +1,5 @@
--- clientesDistintos 
------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Consulta sin indices añadidos
+                                                               QUERY PLAN                                                                
 -----------------------------------------------------------------------------------------------------------------------------------------
  Aggregate  (cost=5240.13..5240.14 rows=1 width=8)
    ->  Gather  (cost=1000.28..5239.37 rows=303 width=4)
@@ -9,12 +9,14 @@
                      Filter: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
                ->  Index Only Scan using customers_pkey on customers c  (cost=0.29..3.66 rows=1 width=4)
                      Index Cond: (customerid = o.customerid)
-(8 filas)
+(8 rows)
+Planning time: 0.389 ms
+Execution time: 14.309 ms
 
 
-
--- clientesDistintos index orderdate
-----------------------------------------------------------------------------------------------------------------------------
+Consulta con indice en orderdate
+                                                QUERY PLAN                                                
+----------------------------------------------------------------------------------------------------------
  Aggregate  (cost=2174.83..2174.84 rows=1 width=8)
    ->  Hash Join  (cost=691.68..2174.08 rows=303 width=4)
          Hash Cond: (o.customerid = c.customerid)
@@ -25,10 +27,13 @@
                      Index Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
          ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
                ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
-(10 filas)
+(10 rows)
+Planning time: 0.292 ms
+Execution time: 8.976 ms
 
 
--- clientesDistintos index totalamount
+Consulta con indice en totalamount
+                                           QUERY PLAN                                            
 -------------------------------------------------------------------------------------------------
  Aggregate  (cost=4546.07..4546.08 rows=1 width=8)
    ->  Hash Join  (cost=1797.07..4545.31 rows=303 width=4)
@@ -40,32 +45,45 @@
                      Index Cond: (totalamount > '100'::numeric)
          ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
                ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
-(10 filas)
+(10 rows)
+Planning time: 0.240 ms
+Execution time: 23.480 ms
 
--- clientesDistintos index orderdate, totalamount
--------
+
+Consulta con indice multicolumna como (orderdate, totalamount)
+                                                                 QUERY PLAN                                                                  
+---------------------------------------------------------------------------------------------------------------------------------------------
  Aggregate  (cost=1480.42..1480.43 rows=1 width=8)
    ->  Hash Join  (cost=697.95..1479.66 rows=303 width=4)
          Hash Cond: (o.customerid = c.customerid)
          ->  Bitmap Heap Scan on orders o  (cost=27.86..808.78 rows=303 width=4)
                Recheck Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
-               ->  Bitmap Index Scan on idx_doble  (cost=0.00..27.78 rows=303 width=0)
+               ->  Bitmap Index Scan on idx_orderdate_totalamount  (cost=0.00..27.78 rows=303 width=0)
                      Index Cond: ((orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date) AND (totalamount > '100'::numeric))
          ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
                ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
-(9 filas)
+(9 rows)
+Planning time: 0.217 ms
+Execution time: 7.601 ms
 
--- clientesDistintos index totalamount, orderdate
+
+Consulta con indice multicolumna como (totalamount, orderdate)
+                                                                 QUERY PLAN                                                                  
+---------------------------------------------------------------------------------------------------------------------------------------------
  Aggregate  (cost=3150.52..3150.53 rows=1 width=8)
    ->  Hash Join  (cost=2368.05..3149.76 rows=303 width=4)
          Hash Cond: (o.customerid = c.customerid)
          ->  Bitmap Heap Scan on orders o  (cost=1697.96..2478.88 rows=303 width=4)
                Recheck Cond: ((totalamount > '100'::numeric) AND (orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
-               ->  Bitmap Index Scan on idx_doble2  (cost=0.00..1697.88 rows=303 width=0)
+               ->  Bitmap Index Scan on idx_totalamount_orderdate  (cost=0.00..1697.88 rows=303 width=0)
                      Index Cond: ((totalamount > '100'::numeric) AND (orderdate < '2015-05-01'::date) AND (orderdate >= '2015-04-01'::date))
          ->  Hash  (cost=493.93..493.93 rows=14093 width=4)
                ->  Seq Scan on customers c  (cost=0.00..493.93 rows=14093 width=4)
-(9 filas)
+(9 rows)
+Planning time: 0.236 ms
+Execution time: 16.768 ms
+
+
 
 
 
